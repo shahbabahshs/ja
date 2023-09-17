97bae62640e28b101431cb559428eee8aef0c6f4
aiauahahsjsushsbs <123171566+shahbabahshs@users.noreply.github.com>
2023-09-18 03:51:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def tiktok():
     return jsonify({"error": "URL is required"}), 400
   
   ssstik_result = ssstik(url)
-  return jsonify(f'{ssstik_result[0]}')
+  return f'{ssstik_result[0]}'
 
   
 @app.route('/api/instagram', methods=["GET"])
